71bd3c77c98d85fff395c791e7500834d907eda0
Hong Quan Nguyen <quannguyen2281995@gmail.com>
2022-10-21 21:33:06 +0200

    init server side code

---
+++ server/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  await app.listen(5050);
+}
+
+// 
+bootstrap();

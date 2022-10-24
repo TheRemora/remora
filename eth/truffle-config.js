7d3b450f889b689cf69774e95ce15cd4231f9e66
trann <trinhtran2151995@gmail.com>
2022-10-24 22:34:29 +0200

    first runnable contracts

--- eth/truffle-config.js
+++ eth/truffle-config.js
@@ -51,23 +51,6 @@ module.exports = {
             network_id: "*", // Any network (default: none)
             confirmations: 0,
         },
-
-        // Useful for deploying to a public network.
-        // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-        //goerli: {
-        //    provider: () => new HDWalletProvider(privateKeys, GOERLI_RPC_URL),
-        //    network_id: 5, // Goerli's id
-        //    confirmations: 1, // # of confirmations to wait between deployments. (default: 0)
-        //    timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-        //    skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-        //},
-        //rinkeby: {
-        //    provider: () => new HDWalletProvider(privateKeys, RINKEBY_RPC_URL),
-        //    network_id: 4, // Rinkeby's id
-        //    confirmations: 1, // # of confirmations to wait between deployments. (default: 0)
-        //    timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-        //    skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-        //},
     },
 
     // Set default mocha options here, use special reporters, etc.

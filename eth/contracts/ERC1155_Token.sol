12253b01a8bbccaf7b6cf6ec38c79998ce86a2a0
trann <trinhtran2151995@gmail.com>
2022-11-05 19:39:33 +0100

    Change versions

--- eth/contracts/ERC1155_Token.sol
+++ eth/contracts/ERC1155_Token.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+
+pragma solidity >=0.6.12;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
